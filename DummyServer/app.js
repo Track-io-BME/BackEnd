109db5295f9b38f76b7d3eba82f805540ed208fe
FogtiIstvan <fogtiistvan47@gmail.com>
2022-10-23 12:22:58 +0200

    auth

---
+++ DummyServer/app.js
@@ -0,0 +1,46 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const https = require('https')
+const fs = require('fs')
+const bcrypt = require('bcrypt')
+const {body} = require('express-validator/check')
+
+const adminRoutes = require('./routes/admin')
+const challangeRoutes = require('./routes/challenges')
+const userdetRoutes = require('./routes/userdetails')
+const userworkRoutes = require('./routes/userworkout')
+const authRoutes = require('./controllers/auth')
+const isAuth = require('./middleware/is-auth')
+
+const users = []
+const app = express()
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}
+
+
+
+app.use('/', (req, res, next)=>{
+  console.log("incoming request")
+  next()
+})
+
+app.post('/', bodyParser.urlencoded({extended: false}))
+
+
+app.use(authRoutes)
+app.use('/userdetails', isAuth, userdetRoutes)
+app.use('/userWorkout', isAuth, userworkRoutes)
+app.use('/challanges', isAuth , challangeRoutes)
+app.use('/admin', isAuth, adminRoutes)
+
+app.use((error, req, res, next)=>{
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({message: message, data: data});
+})
+
+https.createServer(options, app).listen(443)
---
+++ DummyServer/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {body} = require('express-validator/check')
+const authCont = require('../controllers/auth')
+
+const router = express.Router();
+
+router.post('/signup', [
+    body('email').isEmail().withMessage("Please enter a vald email."), 
+    body('password').trim().isLength({min: 5}),
+    body('firstname').trim().not().isEmpty(),
+    body('lasttname').trim().not().isEmpty()
+], authCont.Signup);
+
+router.post('/login',authCont.login);
+
+module.exports = router;
