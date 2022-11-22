474ecbe150df11ec9705d394b2e7ad5324524cc4
FogtiIstvan <fogtiistvan47@gmail.com>
2022-11-22 21:00:59 +0100

    auth

--- Server/middleware/is-auth.js
+++ Server/middleware/is-auth.js
@@ -4,25 +4,33 @@ const Users = require('../models/user');
 
 module.exports = async (req, res, next)=>{
     const authHeader = req.get('Authorization');
-
-    if(!authHeader){
-        const err = new Error('Not authenticated');
-        err.statusCode = 401;
-        throw err;
-    }
-    const token = authHeader.split(' ')[1];
     let decodedToken;
-    try{
-        decodedToken = jwt.verify(token, 'secret');
-    }catch(err){
-        err.statusCode = 500;
-        throw err;
+    
+    try {
+        if(!authHeader){
+            const err = new Error('Not authenticated');
+            err.statusCode = 401;
+            throw err;
+        }
+        const token = authHeader.split(' ')[1];
+        
+        try{
+            decodedToken = jwt.verify(token, 'secret');
+        }catch(err){
+            err.statusCode = 500;
+            throw err;
+        }
+        if(!decodedToken){
+            const error = new Error('Not authenticated');
+            error.statusCode = 401;
+            throw error;
+        }
+    } catch (error) {
+        next(error)
     }
-    if(!decodedToken){
-        const error = new Error('Not authenticated');
-        error.statusCode = 401;
-        throw error;
-    }
+    
+
+
     const id = decodedToken.userID;
     
     req.user = await Users.findOne({
